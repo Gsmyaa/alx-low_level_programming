0671eabbb36b92ce80fce60dfad3ac41d6de2ad1
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-12 12:16:19 -0800

    printout lowercase alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print out lowercase alphabet
+ *Return: 0 value
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+		putchar(\n);
+	}
+	return (0);
+}
