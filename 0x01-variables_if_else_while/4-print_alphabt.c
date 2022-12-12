452499d91072b30614ac1b179ff350856a7fbd13
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-12 13:34:17 -0800

    updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - this fuction exclude e and q from alpha list
+ *Return: 0 value
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		if (l != 'e' || l != 'q')
+			putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
