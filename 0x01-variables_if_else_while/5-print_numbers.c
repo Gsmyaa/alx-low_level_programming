2182cf7042d79bb3a889cf77805efd20805a86fb
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-12 14:07:15 -0800

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - this function
+ *Return: 0 value
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
