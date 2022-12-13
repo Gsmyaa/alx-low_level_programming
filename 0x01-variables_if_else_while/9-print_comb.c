b4ef9916dc18595b83dbcea6a5b9ad494f6811f4
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-13 10:35:36 -0800

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,13 @@
  */
 int main(void)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (i < 9)
+	while (i <= 9)
 	{
-		while (j < 9)
-		{
-			putchar(i + '0');
-			putchar(','' ');
-			putchar(j + '0');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
