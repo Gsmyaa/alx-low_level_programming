9aefe5ee794f1cc57acad8f2ceb5b589a685b58e
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-14 10:21:53 -0800

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ *main - call function
+ *Return: 0 value
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
  *print_alphabet - a function that print lower case
  *
  *Return: void value
