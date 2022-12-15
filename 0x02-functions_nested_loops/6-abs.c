c97905fdb6f6f4b2e41bd81dd43e29356138c11d
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-14 20:13:27 -0800

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - absolute value
+ *@n: take this value and compute it abs
+ *Return: 0 value
+ */
+int _abs(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		a = (-1) * n;
+		return (a);
+	}
+	if (n >= 0)
+	{
+		return (n);
+	}
+}
