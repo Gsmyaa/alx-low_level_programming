ea0a67e53b09eae62a06179be4cd9b18f331da3c
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-15 08:42:13 -0800

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
  *jack_bauer - print every minute of day
+ *@int: input
  *Return: void
+ *
  */
+void pass1(int);
+void pass2(int);
 void jack_bauer(void)
 {
 	int i, j, k, l;
@@ -11,38 +15,57 @@ void jack_bauer(void)
 	{
 		if (i == 2)
 		{
-			for (j = 0; j <= 3; j++)
-			{
-				for (k = 0; k <= 5; k++)
-				{
-					for (l = 0; l <= 9; l++)
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(':');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			pass1(i);
 		}
 		else
 		{
-			for (j = 0; j <= 9; j++)
+			pass2(i)
+		}
+	}
+}
+/**
+ *pass1 - it takes value of 2
+ *@i: iput fro main
+ *Return: void
+ */
+void pass1(int i)
+{
+	for (j = 0; j <= 3; j++)
+	{
+		for (k = 0; k <= 5; k++)
+		{
+			for (l = 0; l <= 9; l++)
 			{
-				for (k = 0; k <= 5; k++)
-				{
-					for (l = 0; l <= 9; l++)
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(':');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
+
+			}
+		}
+	}
+}
+/**
+ *pass2 - it takse value otherthan 2
+ *@i: input value from main
+ *Return: void
+ */
+void pass2(int i)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 5; k++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+				_putchar('\n');
 			}
 		}
 	}
