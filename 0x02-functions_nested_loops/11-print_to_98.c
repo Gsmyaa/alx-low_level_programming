59e978ec17d53abf816b57ddc8be06d1df11ad34
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-17 09:23:49 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; n < 98; n--)
+		for (i = n; n < 98; i--)
 		{
 			if (i == 98)
 			{
@@ -26,7 +26,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; n > 98; n++)
+		for (i = n; n > 98; i++)
 		{
 			if (i == 98)
 			{
