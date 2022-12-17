6dca2aa8fba0568a2beeacf7918a6ddf0ef30b1d
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-17 06:33:40 -0800

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,17 +14,32 @@ void times_table(void)
 			k = i * j;
 			if (k > 9)
 			{
+				if (j == 9)
+				{
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				}
 			}
 			else
 			{
-				_putchar(k + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (j == 9)
+				{
+					_putchar(k + '0');
+				}
+				else
+				{
+					_putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
