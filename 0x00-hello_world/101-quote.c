ff69cf7bb3dfdef43cb6bae2cc87cce92e8016a7
Gsmyaa <gedasirajmohammed@gmail.com>
2022-12-18 09:19:55 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - print out string
  *Return: 1 value
  */
 int main(void)
 {
-	int i;
-	char c;
-	char string[] = { "and that piece of art is \
-		useful\" - Dora Korpar, 2015-10-19"};
-	for (i = 0; i < 58; i++)
-	{
-		c = string[i];
-		putchar(c);
-	}
+	int c;
+
+	c = write(1, "and that piece of art is \
+			useful\" - Dora Korpar, 2015-10-19", 58);
 	putchar('\n');
+	return (0);
 }
