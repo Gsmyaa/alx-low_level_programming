4f3456adc52c85bfde56530ce67d58cb5182877d
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2022-12-23 01:44:07 +0300

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,22 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, len = 0;
+	char *med = dest;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[j] != '\0')
+	{
+		len++;
+	}
+	if (n > len)
+	{
+		n = len;
+	}
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	return (dest);
+	return (med);
 
 }
