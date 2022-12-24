0512697eb8fd460f07280dd957302b75cd453223
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2022-12-24 20:33:01 +0300

    task 6 done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int check_symbol(char c);
 /**
  *cap_string - capitalize all words of string
  *@arr: input arrya
@@ -10,20 +11,37 @@ char *cap_string(char *arr)
 
 	for (i = 0; arr[i] != '\0'; i++)
 	{
-		if (arr[i] == ' ' || arr[i] == '.' || arr[i] == '\n'
-			|| arr[i] == ';' || arr[i] == ',' ||
-			arr[i] == '!' || arr[i] == '?' || arr[i] == '"' || arr[i] == '(' ||
-			arr[i] == ')' || arr[i] == '{' || arr[i] == '}')
+		if (i == 0; &&(arr[i] >= 'a' && arr[i] <= 'z'))
+		{
+			arr[i] = arr[i] - 32;
+		}
+		if (check_symbol(arr[i]))
 		{
 			if (arr[i + 1] >= 'a' && arr[i + 1] <= 'z')
 			{
 				arr[i + 1] = arr[i + 1] - 32;
 			}
-			else
-			{
-				arr[i + 1] = arr[i + 1];
-			}
 		}
 	}
 	return (arr);
+}
+/**
+ *check_symbol - checks symbol for seperators
+ *@c: input char from cap_string function
+ *Return: int value to cap_string function
+ */
+int check_symbol(char c)
+{
+	int i;
+	char symbol[] = {',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}', '\n', '\t', ' '};
+
+	for (i = 0; symbol[i]; i++)
+	{
+		if (c == symbol[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
