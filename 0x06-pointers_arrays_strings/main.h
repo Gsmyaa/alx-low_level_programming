eb35e95823cfa9433c0be838fca5019eb5f51e39
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2022-12-26 03:41:40 +0300

    task 8 done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ char *cap_string(char *arr);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
-char *rot13(char *);
+char *rot13(char *c);
 #endif
