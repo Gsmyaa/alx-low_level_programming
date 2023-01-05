9edc0e61dfc2d08764018e0f8a1591d966aa07a2
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-05 07:02:26 +0300

    tasd 1 done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - reverse
+ *@s: input
+ *Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
