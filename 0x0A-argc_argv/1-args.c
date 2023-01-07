5c442f4e5e9b705b1a644cb3bdf800ab156ecceb
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-07 12:47:19 +0300

    task 1 done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,9 @@
  *main - prints the number of arguments
  *@argc: input argument number
  *@argv: input argument array
+ *Return: int value
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
