91fd184b2c10343a7fac2ba80a614dbc5855fa5e
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-07 12:28:56 +0300

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *main - prints the number of arguments
+ *@argc: input argument number
+ *@argv: input argument array
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
