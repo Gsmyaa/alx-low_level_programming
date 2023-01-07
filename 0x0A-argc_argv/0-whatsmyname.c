f451bf6d13092989f71236125cc32288f5a8c921
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-07 05:31:09 +0300

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - main
+ *@argc: input agr number
+ *@argv: input array
+ *Return: int value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+these are tasks for argc argv
