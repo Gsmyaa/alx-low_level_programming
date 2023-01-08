7ccbb8e1c526558c81ab9a184a6807d6a7f3b01d
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-08 22:55:59 +0300

    task 4 done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_pow_recursion - returns value of x raised to y
+ *@x: input
+ *@y: input
+ *Return: int value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
