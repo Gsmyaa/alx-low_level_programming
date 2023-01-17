3d80ee0906b5354cc0ed533a3d254628fabe716c
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-18 02:08:37 +0300

    task done

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  *free_dog - frees dogs
  *@d: input structires
@@ -6,5 +7,9 @@
  */
 void free_dog(dog_t *d);
 {
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
 	free(d);
 }
