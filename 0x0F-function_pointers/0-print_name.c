3a83d13b6433ee955e277efabb0bd4266085afa6
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-18 22:26:31 +0300

    task 0 done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints name
+ *@name: input char
+ *Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || f)
+	{
+		f(name);
+	}
+	else
+		return;
+}
