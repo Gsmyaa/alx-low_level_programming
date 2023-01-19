34898a5c545705bd49d65cb5c2079549e7365206
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-20 00:20:58 +0300

    task 1 is done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < n - 1)
 		{
 			j = va_arg(lists, int);
-			printf("%d, ", j);
+			printf("%d%s", j, separator);
 		}
 		else
 		{
