98a7ea6b504bee925ef482ade98057bddebdf8fd
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-20 00:08:43 +0300

    task 1 is done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_numbers - prints numbers
+ *@separator: input string
+ *@n: input paramaters
+ *Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, j;
+	va_list lists;
+
+	if (separator == NULL)
+		return;
+	va_start(lists, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			j = va_arg(lists, int);
+			printf("%d, ", j);
+		}
+		else
+		{
+			j = va_arg(lists, int);
+			printf("%d", j);
+		}
+	}
+	putchar('\n');
+	va_end(lists);
+}
