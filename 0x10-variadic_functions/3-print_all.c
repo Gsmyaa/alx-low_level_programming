d51e21789941f100b3810bebf5cf119b853d5a03
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-21 17:44:59 +0300

    task 3 done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(lists, char *);
 				if (str == NULL)
-					printf("(nil)");
+					printf("nil");
 				printf("%s", str);
 				break;
 			default:
