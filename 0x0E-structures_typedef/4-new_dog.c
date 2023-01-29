4e629d16e45e7e967330ee320f0b3f5c4817f467
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-29 22:39:21 +0300

    adding malloc in the code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  *new_dog - creates a new dog
  *@name: input nme
@@ -14,8 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	(*ptr).name = name;
+	(*ptr).name = strdup(name);
 	(*ptr).age = age;
-	(*ptr).owner = owner;
+	(*ptr).owner = strdup(owner);
 	return (ptr);
 }
