ddc138dddf3298f8d624099df6b4c801c5ba449f
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-31 00:22:47 +0300

    task 4 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees linked list
+ *@head: input address of linked list
+ *Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new, *temp;
+
+	if (head == NULL)
+		return;
+	new = head;
+	while (new)
+	{
+		temp = new;
+		new = new->next;
+		free(temp);
+	}
+	*head = NULL;
+}
