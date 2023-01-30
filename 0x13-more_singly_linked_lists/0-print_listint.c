72431c6535bf37440611e26791666921b13b10c5
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-30 21:12:27 +0300

    task 0 done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h)
+	while (h)
 	{
-		while (h->next)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
