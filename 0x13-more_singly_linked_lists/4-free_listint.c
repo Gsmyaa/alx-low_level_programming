3637ef659ec75d4295b5cfe71f5fe429a744ee05
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-30 22:57:13 +0300

    task 4 done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ *free_listint - frees a linked list
+ *@head: input address of linked list
+ *Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free(head->next);
+		free(head);
+	}
+}
