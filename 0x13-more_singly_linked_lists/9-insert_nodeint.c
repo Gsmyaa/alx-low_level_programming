b6660c3ed8efc7c7d8be34b1e5634c19bdae4ae4
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-01-31 19:39:36 +0300

    task 9 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0; temp; i++)
 	{
-		if (i == (int) idx)
+		if (i == (int) idx -1)
 		{
 			current = temp->next;
 			temp->next = new;
