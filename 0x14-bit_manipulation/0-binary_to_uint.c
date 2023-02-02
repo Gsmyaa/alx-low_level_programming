65a1030543a1591c3fe4cfa381aa705e5f06c52a
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-03 02:46:38 +0300

    task 0 done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 /**
  *binary_to_uint - converts a binary to unsigned int
  *@b: input pointer
@@ -10,15 +9,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
-	int bin, i;
-
-	bin = atoi(b);
-	for (i = 0; bin; i++, bin /= 10)
+	int bin, rem, base = 1, i = 0;
+	
+	if (!b)
+		return (0);
+	while (b[i] != '\0')
 	{
-		if (bin % 10)
-		{
-			dec += pow(2, i);
-		}
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	bin = atoi(b);
+	while (bin)
+	{
+		rem = bin % 10;
+		dec = dec + rem * base;
+		bin /= 10;
+		base = base * 2;
 	}
 	return (dec);
 }
