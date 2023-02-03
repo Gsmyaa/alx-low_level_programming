94a71065f12d0ccf46060d412fac7969c2973f37
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-03 03:37:57 +0300

    task 4 done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 /**
-  * clear_bit - sets a bit at index to zero
-  * @n: pointer to number
-  * @index: index to change
+  * clear_bit - make a bit at index to zero
+  * @n: input pointer
+  * @index: input number
   * Return: 1 on success, -1 on failure
   */
 int clear_bit(unsigned long int *n, unsigned int index)
