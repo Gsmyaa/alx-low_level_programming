40da3dda95ad5ea9592b95e7376603762c155d4a
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-08 01:19:06 +0300

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,11 @@ int create_file(const char *filename, char *text_content)
 		close(file_d);
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		close(file_d);
+		return (1);
+	}
 	i = write(file_d, text_content, len);
 	if (i < 0)
 	{
