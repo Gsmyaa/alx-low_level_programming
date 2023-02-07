9eb673812b9d9a7a7c8f94b008c89d04431f6ca8
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-08 01:22:35 +0300

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
-	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC | O_EXCL, S_IRUSR | S_IWUSR);
+	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d < 0)
 	{
 		close(file_d);
