d7846961f08d2f0b98407261b0e05be929e877e2
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-08 01:26:27 +0300

    task 0 done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
  *read_textfile - reads text file and prints it to posix stdout
  *@filename: input address of file
- *@letters: numbe of letters to be printed
+ *@letters: number of letters to be printed
  *Return: actual numbers letters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -11,8 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t cnt = 0;
 	char *buf;
 
-	if (!filename)
-		return (0);
 	file_d = open(filename, O_RDONLY);
 	if (file_d < 0)
 	{
@@ -24,14 +29,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file_d);
 		return (0);
 	}
-	cnt = read(file_d, bf, letters);
+	cnt = read(file_d, buf, letters);
 	if (cnt < 0)
 	{
 		free(buf);
 		close(file_d);
 		return (0);
 	}
-	cont = write(STDOUT_FILENO, buf, cnt);
+	cnt = write(STDOUT_FILENO, buf, cnt);
 	free(buf);
 	close(file_d);
 	if (cnt < 0)
