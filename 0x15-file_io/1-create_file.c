d94b1083306bcaca6df152b1159791eb4847b1dc
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-02-08 01:13:29 +0300

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,8 +7,9 @@
  *Return: 1 on succes, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
-{	int file_d, i;
-	
+{
+	int file_d, i;
+
 	if (filename == NULL)
 		return (-1);
 	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
