edd2873d8d8b6fca90520bf697c5005cdb160d70
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-30 13:00:43 +0300

    task 0 done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,12 +3,13 @@
 /**
  *print_dlistint - prints all elements of doubly-linked list
  *@h: pointer value
- *return: the number of nodes
+ *Return: the number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
