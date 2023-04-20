9b10ac688273b78d744f35690471c22dd6fa7317
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-20 15:37:20 +0300

    task 0 done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,18 +7,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t *));
 	unsigned long int i;
 
 	table->size = size;
 	if (table == NULL)
 		return (NULL);
-	table->array = calloc(table->size, sizeof(hash_node_t *));
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-		return (NULL);
-	for (i = 0; i < table->size; i++)
 	{
-		table->array[i] = NULL;
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		(table->array)[i] = NULL;
 	}
 	return (table);
 }
