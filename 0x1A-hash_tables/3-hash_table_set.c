cddc2c73aa427b395d77f844c2828de0ce15e3ef
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-21 02:44:19 +0300

    task 3 done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,56 +1,76 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_set - adds an element to the hash table
- *@ht: hash table to be added or updated
- *@key: the key, cannot be empty
- *@value: the value associated with the key
- *Return: 1 if it succeeded, 0 otherwise
- */
-
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table.
+ * @key: key.
+ * @value: value associated with the key.
+ * Return: 1 if it succeeded, 0 otherwise.
+ **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *tmp;
-	char *value_;
+	unsigned long int index = 0;
+	char *value_ = NULL, *key_ = NULL;
+	hash_node_t *new_node = NULL;
 
-	if (!ht || !key)
+	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	new_node->key = strdup(key);
-	if (!new_node->key)
+	key_ = strdup(key);
+	if (!key_)
 	{
 		free(new_node);
 		return (0);
 	}
-	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = NULL;
-	tmp = ht->array[index];
 	value_ = strdup(value);
-	while (tmp)
+	if (!value_)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		free(new_node);
+		free(key_);
+		return (0);
+	}
+	new_node->key = key_;
+	new_node->value = value_;
+	new_node->next = NULL;
+	index = key_index((unsigned char *)key, ht->size);
+	if (!((ht->array)[index]))
+		(ht->array)[index] = new_node;
+	else
+		hash_table_update(ht, key_, value_, index, new_node);
+	return (1);
+}
+
+/**
+ * hash_table_update - adds an element to the hash table.
+ * @ht: hash table.
+ * @key_: key.
+ * @value_: value associated with the key.
+ * @index: index in the hash table to insert/update the node.
+ * @new_node: node to insert into hash table.
+ * Return: Always 1.
+ **/
+int hash_table_update(hash_table_t *ht, char *key_, char *value_,
+						unsigned long int index, hash_node_t *new_node)
+{
+	hash_node_t *temp_node = (ht->array)[index];
+
+	while (temp_node)
+	{
+		if (strcmp(temp_node->key, key_) == 0)
 		{
+			free(key_);
 			free(new_node);
 			free(ht->array[index]->value);
 			ht->array[index]->value = value_;
 			return (1);
 		}
-		tmp = tmp->next;
+		temp_node = temp_node->next;
 	}
-	tmp = ht->array[index];
-	new_node->next = tmp;
-	ht->array[index] = new_node;
+	temp_node = (ht->array)[index];
+	new_node->next = temp_node;
+	(ht->array)[index] = new_node;
 	return (1);
-
-
 }
