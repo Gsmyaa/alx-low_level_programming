0d0121d1526b4b0b83c5c65dbaeb2b6f7c5c35a1
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-21 04:55:37 +0300

    task 6 done

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - deletes a hash table
+ *@ht: the hash table
+ *Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *tmp2;
+	unsigned int i;
+
+	if(!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			tmp2 = tmp;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
+			tmp = tmp->next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
