cc8c66f75205121a12f2132e366f173cb749a23a
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-21 04:38:42 +0300

    task 5 done

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,13 +8,12 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, end = 0;
 	hash_node_t *tmp;
-	
+
 	if (ht)
 	{
 		printf("{");
 		for (i = 0; i < ht->size; i++)
-		{	
-			
+		{
 			tmp = ht->array[i];
 			while (tmp)
 			{
